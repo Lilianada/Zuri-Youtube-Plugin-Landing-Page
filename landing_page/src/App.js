d64b3bbd2944798b9e272b8fa7ce81bcf27f594c
Lilianada <lilibest30.lb@gmail.com>
2021-09-26 17:04:09 +0100

    message

---
+++ landing_page/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './Frame 8866.svg';
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
